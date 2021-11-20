e6dc27381739b09d8d2da06679e374a3589ed7a3
Riki Firmansyah <59858465+rizkyfirman15@users.noreply.github.com>
2021-11-20 14:26:53 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -249,7 +249,7 @@ const achievementSection = {
 
     {
       title: "UI/UX Design Mastery - PROA",
-      subtitle: "The Professional Academy program aims to prepare for the transfer of Indonesian HR competencies by providing online training to working people in order to create a more adaptive and productive Indonesian workforce.",
+      subtitle: "The Professional Academy program aims to prepare for the transfer of Indonesian HR competencies. This program is organized by Kominfo.",
       image: require("./assets/images/skilvul.svg"),
       footerLink: [
         {
